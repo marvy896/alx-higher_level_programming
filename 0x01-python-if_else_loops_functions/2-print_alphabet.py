467cf8cc939abf85286981aaa353126ee767f23e
marvy896 <onyex896@gmail.com>
2023-05-09 16:39:03 +0000

    update

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-# 2-print_alphabet.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-"""Print the alphabet in lowercase, not followed by a new line."""
 for i in 'abcdefghijklmnopqrstuvwxyz':
     print("{:s}".format(i), end="")
