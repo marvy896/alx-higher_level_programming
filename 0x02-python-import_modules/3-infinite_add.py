f1dcdf2ba61313f9dc779a13367355820d8e20e2
marvy896 <onyex896@gmail.com>
2023-05-11 19:05:29 +0000

    infinite

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+    argc = len(argv)
+    result = 0
+    for i in range(1, argc):
+        result += int(argv[i])
+    print(f'{result:d}')
