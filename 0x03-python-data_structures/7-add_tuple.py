fcbfc27acb97ae763b1a7bebdd93e9e8742db52f
marvy896 <onyex896@gmail.com>
2023-05-15 20:21:20 +0000

    Turple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Extract the first two elements from each tuple or use 0 if the tuple is smaller
-    a = tuple_a[0] if len(tuple_a) >= 1 else 0
-    b = tuple_a[1] if len(tuple_a) >= 2 else 0
-    c = tuple_b[0] if len(tuple_b) >= 1 else 0
-    d = tuple_b[1] if len(tuple_b) >= 2 else 0
-    
-    # Perform the addition of the corresponding elements
-    result_a = a + c
-    result_b = b + d
-    
-    # Return the new tuple
-    return (result_a, result_b)
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
