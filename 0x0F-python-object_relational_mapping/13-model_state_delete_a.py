904efa1939595ef9cb0af95fcb4098d3cb1c320e
marvy896 <onyex896@gmail.com>
2023-08-20 22:32:22 +0000

    fix2

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-"""
-All states via SQLAlchemy
-"""
-from sys import argv
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import Session
+# delete all State objects with a name containing letter a
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
+    from sqlalchemy.engine import create_engine
+    from sqlalchemy.engine.url import URL
+    from sqlalchemy.orm import Session
+    from model_state import Base, State
+    from sys import argv
+
+    db = {'drivername': 'mysql+mysqldb',
+          'host': 'localhost',
+          'port': '3306',
+          'username': argv[1],
+          'password': argv[2],
+          'database': argv[3]}
+
+    url = URL(**db)
+    engine = create_engine(url, pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    data = session.query(State).filter(State.name.like("%a%"))\
-                               .delete(synchronize_session='fetch')
+    for query in session.query(State).filter(State.name.like('%a%')).all():
+        session.delete(query)
     session.commit()
-    session.close()
